783b4a7ad01abe63ebca29abd843875729db7285
jvits <jscvito@gmail.com>
2018-03-02 11:06:01 +0800

    result

--- php/index.php
+++ php/index.php
@@ -1,5 +1,32 @@
 <?php
 
-echo 'hello world';
+$jsonLink = file_get_contents("http://igovphil.github.io/java-exam-01/uacs-agency.json");
+$jsonUacs = json_decode($jsonLink, true);
+
+if(count($jsonUacs)){
+    echo '<table class="table">';
+                echo '<th>UACS</th>';
+                echo '<th>DEPARTMENT</th>';
+                echo '<th>AGENCY</th>';
+        
+                foreach($jsonUacs as $uacsKey){
+                    echo "<tr>";
+                        echo "<td>";
+                            echo json_encode($uacsKey['uacs']);
+                        echo "</td>";
+
+                        echo "<td>";
+                            echo json_encode($uacsKey['department']);
+                        echo "</td>";
+
+                        echo "<td>";
+                            echo json_encode($uacsKey['agency']);
+                        echo "</td>";
+                    echo "</tr>";
+                }
+
+    echo '</table>';
+}
+
 
 ?>
