19c26e3b3e3002347b1b5631050c9c5d2e1b2a6c
jvits <jscvito@gmail.com>
2018-03-02 13:47:05 +0800

    updated result

--- php/index.php
+++ php/index.php
@@ -3,24 +3,26 @@
 $jsonLink = file_get_contents("http://igovphil.github.io/java-exam-01/uacs-agency.json");
 $jsonUacs = json_decode($jsonLink, true);
 
+
+
 if(count($jsonUacs)){
-    echo '<table class="table">';
+    echo '<table class="table table-striped">';
                 echo '<th>UACS</th>';
                 echo '<th>DEPARTMENT</th>';
                 echo '<th>AGENCY</th>';
         
                 foreach($jsonUacs as $uacsKey){
                     echo "<tr>";
-                        echo "<td>";
-                            echo json_encode($uacsKey['uacs']);
+                        echo "<td style=' border: 1px solid #7f8c8d;'>";
+                            echo preg_replace('/"/','',json_encode($uacsKey['uacs']));
                         echo "</td>";
 
-                        echo "<td>";
-                            echo json_encode($uacsKey['department']);
+                        echo "<td style=' border: 1px solid #7f8c8d;'>";
+                            echo preg_replace('/"/','',json_encode($uacsKey['department']));
                         echo "</td>";
 
-                        echo "<td>";
-                            echo json_encode($uacsKey['agency']);
+                        echo "<td style=' border: 1px solid #7f8c8d;'>";
+                            echo preg_replace('/"/','',json_encode($uacsKey['agency']));
                         echo "</td>";
                     echo "</tr>";
                 }
